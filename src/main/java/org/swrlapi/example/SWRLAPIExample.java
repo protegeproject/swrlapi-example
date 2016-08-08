9c3fa5054b1f52153db4dea45ef667385f0198a8
martinjoconnor <martin.oconnor@stanford.edu>
2016-08-08 11:14:02 -0700

    Minor example cleanup

--- src/main/java/org/swrlapi/example/SWRLAPIExample.java
+++ src/main/java/org/swrlapi/example/SWRLAPIExample.java
@@ -1,5 +1,6 @@
 package org.swrlapi.example;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
@@ -10,20 +11,33 @@ import org.swrlapi.sqwrl.SQWRLQueryEngine;
 import org.swrlapi.sqwrl.SQWRLResult;
 import org.swrlapi.sqwrl.exceptions.SQWRLException;
 
+import java.io.File;
+import java.util.Optional;
+
 public class SWRLAPIExample
 {
   public static void main(String[] args)
   {
+    if (args.length > 1)
+      Usage();
+
+    Optional<@NonNull String> owlFilename = args.length == 0 ? Optional.<@NonNull String>empty() : Optional.of(args[0]);
+    Optional<@NonNull File> owlFile = (owlFilename != null && owlFilename.isPresent()) ?
+      Optional.of(new File(owlFilename.get())) :
+      Optional.<@NonNull File>empty();
+
     try {
-      // Create an empty OWL ontology using the OWLAPI
+      // Create an OWL ontology using the OWLAPI
       OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
-      OWLOntology ontology = ontologyManager.createOntology();
+      OWLOntology ontology = owlFile.isPresent() ?
+        ontologyManager.loadOntologyFromOntologyDocument(owlFile.get()) :
+        ontologyManager.createOntology();
 
       // Create SQWRL query engine using the SWRLAPI
       SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
 
       // Create and execute a SQWRL query using the SWRLAPI
-      SQWRLResult result = queryEngine.runSQWRLQuery("q1","swrlb:add(?x, 2, 2) -> sqwrl:select(?x)");
+      SQWRLResult result = queryEngine.runSQWRLQuery("q1", "swrlb:add(?x, 2, 2) -> sqwrl:select(?x)");
 
       // Process the SQWRL result
       if (result.next())
@@ -33,14 +47,20 @@ public class SWRLAPIExample
       System.err.println("Error creating OWL ontology: " + e.getMessage());
       System.exit(-1);
     } catch (SWRLParseException e) {
-      System.err.println("Error parsing SQWRL query: " + e.getMessage());
+      System.err.println("Error parsing SWRL rule or SQWRL query: " + e.getMessage());
       System.exit(-1);
     } catch (SQWRLException e) {
-      System.err.println("Error running SQWRL query: " + e.getMessage());
+      System.err.println("Error running SWRL rule or SQWRL query: " + e.getMessage());
       System.exit(-1);
     } catch (RuntimeException e) {
       System.err.println("Error starting application: " + e.getMessage());
       System.exit(-1);
     }
+  }
+
+  private static void Usage()
+  {
+    System.err.println("Usage: " + SWRLAPIExample.class.getName() + " [ <owlFileName> ]");
+    System.exit(1);
   }
 }
